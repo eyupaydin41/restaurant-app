9c0d22df2fd57a454c3ea54713698c40f7245a7f
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-09 20:54:15 +0300

    Initial commit Created modules: restaurant-service, user-service, config-server, app-gateway

---
+++ app-gateway/src/test/java/com/restaurant/appgateway/AppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.appgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
