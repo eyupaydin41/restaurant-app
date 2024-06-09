421df3bc2f59b574f73cb454752d7f54f0be7638
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-10 01:48:36 +0300

    Add RestaurantRequest DTO for creating and updating restaurants

---
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/dto/RestaurantRequest.java
@@ -0,0 +1,20 @@
+package com.restaurant.restaurantservice.dto;
+
+import com.restaurant.restaurantservice.model.RestaurantCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestaurantRequest {
+    private String name;
+    private RestaurantCategory category;
+    private String address;
+    private String phone;
+    private String email;
+    private String description;
+}
