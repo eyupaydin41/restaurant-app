d58fe45ffb88fcf92dedc31f85aaf57b875896e4
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-12 03:05:23 +0300

    Add User entity class

---
+++ user-service/src/main/java/com/restaurant/userservice/model/User.java
@@ -0,0 +1,33 @@
+package com.restaurant.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
