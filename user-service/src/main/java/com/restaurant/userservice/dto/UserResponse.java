5ccfb66cf555e7e7d65d4f6c1a6119f46ff05ec7
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-12 03:03:02 +0300

    Add UserResponse DTO class

---
+++ user-service/src/main/java/com/restaurant/userservice/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package com.restaurant.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+}
