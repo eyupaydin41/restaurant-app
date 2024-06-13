d8f3138cef05f0c73527b489a279419d94b241b7
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-13 18:21:47 +0300

    Add validation and error handling for CustomerReviewDTO in CustomerReviewController

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/controller/CustomerReviewController.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/controller/CustomerReviewController.java
@@ -1,13 +1,15 @@
 package com.restaurant.restaurantservice.controller;
 
-import com.restaurant.restaurantservice.dto.CustomerReviewRequest;
-import com.restaurant.restaurantservice.dto.CustomerReviewResponse;
+import com.restaurant.restaurantservice.dto.CustomerReviewDTO;
+import com.restaurant.restaurantservice.exception.InvalidRatingException;
+import com.restaurant.restaurantservice.model.response.Response;
 import com.restaurant.restaurantservice.service.CustomerReviewService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/reviews")
@@ -18,31 +20,37 @@ public class CustomerReviewController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createReview(@RequestBody CustomerReviewRequest customerReviewRequest) {
-        customerReviewService.createReview(customerReviewRequest);
+    public ResponseEntity<Response> createReview(@RequestBody @Valid CustomerReviewDTO customerReviewDTO,BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRatingException();
+        }
+        return customerReviewService.createReview(customerReviewDTO);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void updateReview(@PathVariable Long id, @RequestBody CustomerReviewRequest customerReviewRequest) {
-        customerReviewService.updateReview(id, customerReviewRequest);
+    public ResponseEntity<Response> updateReview(@PathVariable Long id, @RequestBody @Valid CustomerReviewDTO customerReviewDTO,BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRatingException();
+        }
+        return customerReviewService.updateReview(id, customerReviewDTO);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<CustomerReviewResponse> getAllReviews() {
+    public ResponseEntity<Response> getAllReviews() {
         return customerReviewService.getAllReviews();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public CustomerReviewResponse getReviewById(@PathVariable Long id) {
+    public ResponseEntity<Response> getReviewById(@PathVariable Long id) {
         return customerReviewService.getReviewByID(id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteReview(@PathVariable Long id) {
-        customerReviewService.deleteReview(id);
+    public ResponseEntity<Response> deleteReview(@PathVariable Long id) {
+        return customerReviewService.deleteReview(id);
     }
 }
