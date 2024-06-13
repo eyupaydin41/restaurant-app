543132636b0a6f3ba5b2e44373a8287685a29f4f
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-13 18:53:02 +0300

    Add Response class for structured responses

---
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/model/response/Response.java
@@ -0,0 +1,17 @@
+package com.restaurant.restaurantservice.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Response<T> {
+
+    private HttpStatus status;
+    private final String message;
+    private final T data;
+
+}
