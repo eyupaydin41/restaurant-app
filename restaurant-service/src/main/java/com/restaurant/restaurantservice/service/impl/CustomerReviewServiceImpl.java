87b88bf251a5a9267c9ab797ef4b006e1841f430
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-13 18:49:42 +0300

    -Updated CustomerReviewServiceImpl to use ResponseEntity<Response> and handle exceptions -Refactored exception handling to use specific exception classes. -Enhanced updateRestaurantRatings method to round rating averages to one decimal place for consistency.

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/service/impl/CustomerReviewServiceImpl.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/service/impl/CustomerReviewServiceImpl.java
@@ -1,21 +1,26 @@
 package com.restaurant.restaurantservice.service.impl;
 
-import com.restaurant.restaurantservice.dto.CustomerReviewRequest;
-import com.restaurant.restaurantservice.dto.CustomerReviewResponse;
+import com.restaurant.restaurantservice.dto.CustomerReviewDTO;
 import com.restaurant.restaurantservice.exception.DuplicateReviewException;
-import com.restaurant.restaurantservice.exception.ResourceNotFoundException;
+import com.restaurant.restaurantservice.exception.RestaurantNotFoundException;
+import com.restaurant.restaurantservice.exception.ReviewNotFoundException;
 import com.restaurant.restaurantservice.model.CustomerReview;
 import com.restaurant.restaurantservice.model.Restaurant;
+import com.restaurant.restaurantservice.model.response.Response;
+import com.restaurant.restaurantservice.model.response.ResponseBuilder;
 import com.restaurant.restaurantservice.repository.RestaurantRepository;
 import com.restaurant.restaurantservice.repository.CustomerReviewRepository;
 import com.restaurant.restaurantservice.service.CustomerReviewService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.OptionalDouble;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,73 +29,101 @@ public class CustomerReviewServiceImpl implements CustomerReviewService {
     private final CustomerReviewRepository customerReviewRepository;
     private final RestaurantRepository restaurantRepository;
     private final ModelMapper modelMapper;
+    private final ResponseBuilder responseBuilder;
 
     @Override
-    public void createReview(CustomerReviewRequest customerReviewRequest) {
-        Restaurant restaurant = restaurantRepository.findById(customerReviewRequest.getRestaurantId())
-                .orElseThrow(() -> new ResourceNotFoundException("Restaurant not found with id: " + customerReviewRequest.getRestaurantId()));
+    public ResponseEntity<Response> createReview(CustomerReviewDTO customerReviewDTO) {
+        Restaurant restaurant = restaurantRepository.findById(customerReviewDTO.getRestaurantId())
+                .orElseThrow(() -> new RestaurantNotFoundException(customerReviewDTO.getRestaurantId()));
 
-        // customerReviewRequest.getUserId() -> user mevcut mu kontrolü yapılacak
+        // customerReviewDTO.getUserId() -> user mevcut mu kontrolü yapılacak
 
-        customerReviewRepository.findByUserIdAndRestaurantId(customerReviewRequest.getUserId(), customerReviewRequest.getRestaurantId())
-                .orElseThrow(() -> new DuplicateReviewException("User has already reviewed this restaurant"));
+        customerReviewRepository.findByUserIdAndRestaurantId(customerReviewDTO.getUserId(), customerReviewDTO.getRestaurantId())
+                .ifPresent(review -> {
+                    throw new DuplicateReviewException();
+                });
 
 
         CustomerReview customerReview = CustomerReview.builder()
-                .serviceRating(customerReviewRequest.getServiceRating())
-                .tasteRating(customerReviewRequest.getTasteRating())
-                .priceRating(customerReviewRequest.getPriceRating())
-                .comment(customerReviewRequest.getComment())
+                .serviceRating(customerReviewDTO.getServiceRating())
+                .tasteRating(customerReviewDTO.getTasteRating())
+                .priceRating(customerReviewDTO.getPriceRating())
+                .comment(customerReviewDTO.getComment())
                 .restaurant(restaurant)
-                .userId(customerReviewRequest.getUserId())
+                .userId(customerReviewDTO.getUserId())
                 .build();
 
         customerReviewRepository.save(customerReview);
         updateRestaurantRatings(restaurant);
+
+        String message = "Successfully created the customer review.";
+        return responseBuilder.buildResponse(message, HttpStatus.CREATED, customerReviewDTO);
+
     }
 
 
     @Override
-    public List<CustomerReviewResponse> getAllReviews() {
-        return customerReviewRepository.findAll()
+    public ResponseEntity<Response> getAllReviews() {
+        List<CustomerReviewDTO> list = customerReviewRepository.findAll()
                 .stream()
                 .map(this::entityToDto)
-                .collect(Collectors.toList());
+                .toList();
+
+        String message = "The customer review list has been successfully fetched.";
+        return responseBuilder.buildResponse(message, HttpStatus.OK, list);
     }
 
     @Override
-    public CustomerReviewResponse getReviewByID(Long id) {
-        return customerReviewRepository.findById(id)
+    public ResponseEntity<Response> getReviewByID(Long id) {
+        CustomerReviewDTO response = customerReviewRepository.findById(id)
                 .map(this::entityToDto)
-                .orElseThrow(() -> new ResourceNotFoundException("CustomerReview not found with id: " + id));
+                .orElseThrow(() -> new ReviewNotFoundException(id));
+
+        String message = "Custoemr review found with id: " + id;
+
+        return responseBuilder.buildResponse(message, HttpStatus.OK, response);
+
     }
 
     @Override
-    public void updateReview(Long reviewId, CustomerReviewRequest customerReviewRequest) {
+    public ResponseEntity<Response> updateReview(Long reviewId, CustomerReviewDTO customerReviewDTO) {
         CustomerReview customerReview = customerReviewRepository.findById(reviewId)
-                .orElseThrow(() -> new ResourceNotFoundException("CustomerReview not found with id: " + reviewId));
+                .orElseThrow(() -> new ReviewNotFoundException(reviewId));
 
-        customerReview.setServiceRating(customerReviewRequest.getServiceRating());
-        customerReview.setTasteRating(customerReviewRequest.getTasteRating());
-        customerReview.setPriceRating(customerReviewRequest.getPriceRating());
-        customerReview.setComment(customerReviewRequest.getComment());
+        customerReview.setServiceRating(customerReviewDTO.getServiceRating());
+        customerReview.setTasteRating(customerReviewDTO.getTasteRating());
+        customerReview.setPriceRating(customerReviewDTO.getPriceRating());
+        customerReview.setComment(customerReviewDTO.getComment());
 
         customerReviewRepository.save(customerReview);
         updateRestaurantRatings(customerReview.getRestaurant());
+
+        String message = "Successfully updated customer review with ID: " + reviewId;
+
+        return responseBuilder.buildResponse(message, HttpStatus.OK, customerReviewDTO);
+
+
     }
 
     @Override
-    public void deleteReview(Long reviewId) {
+    public ResponseEntity<Response> deleteReview(Long reviewId) {
         CustomerReview customerReview = customerReviewRepository.findById(reviewId)
-                .orElseThrow(() -> new ResourceNotFoundException("CustomerReview not found with id: " + reviewId));
+                .orElseThrow(() -> new ReviewNotFoundException(reviewId));
+
+        CustomerReviewDTO customerReviewDTO = entityToDto(customerReview);
 
         customerReviewRepository.delete(customerReview);
         updateRestaurantRatings(customerReview.getRestaurant());
+
+        String message = "Successfully updated customer review with ID: " + reviewId;
+
+        return responseBuilder.buildResponse(message, HttpStatus.OK, customerReviewDTO);
+
     }
 
 
-    public CustomerReviewResponse entityToDto(CustomerReview customerReview) {
-        return modelMapper.map(customerReview, CustomerReviewResponse.class);
+    public CustomerReviewDTO entityToDto(CustomerReview customerReview) {
+        return modelMapper.map(customerReview, CustomerReviewDTO.class);
     }
 
     private void updateRestaurantRatings(Restaurant restaurant) {
@@ -99,12 +132,20 @@ public class CustomerReviewServiceImpl implements CustomerReviewService {
         OptionalDouble serviceRatingAverage = customerReviews.stream().mapToInt(CustomerReview::getServiceRating).average();
         OptionalDouble tasteRatingAverage = customerReviews.stream().mapToInt(CustomerReview::getTasteRating).average();
         OptionalDouble priceRatingAverage = customerReviews.stream().mapToInt(CustomerReview::getPriceRating).average();
+        OptionalDouble averageRating = customerReviews.stream().mapToDouble(CustomerReview::getAverageRating).average();
 
-        restaurant.setServiceRatingAverage(serviceRatingAverage.orElse(0.0));
-        restaurant.setTasteRatingAverage(tasteRatingAverage.orElse(0.0));
-        restaurant.setPriceRatingAverage(priceRatingAverage.orElse(0.0));
+        restaurant.setServiceRatingAverage(roundToOneDecimalPlace(serviceRatingAverage.orElse(0.0)));
+        restaurant.setTasteRatingAverage(roundToOneDecimalPlace(tasteRatingAverage.orElse(0.0)));
+        restaurant.setPriceRatingAverage(roundToOneDecimalPlace(priceRatingAverage.orElse(0.0)));
+        restaurant.setRatingAverage(roundToOneDecimalPlace(averageRating.orElse(0.0)));
 
         restaurantRepository.save(restaurant);
+    }
+
+    private Double roundToOneDecimalPlace(Double value) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(1, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
 }
