5ce3a17cec17fb63c7af42f88fdd0ca32e78e69f
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-15 16:16:54 +0300

    Add gateway configuration for API routing

--- app-gateway/src/main/java/com/gateway/AppGatewayApplication.java
+++ app-gateway/src/main/java/com/gateway/AppGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.restaurant.appgateway;
+package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppGatewayApplication.class, args);
+    }
 }
