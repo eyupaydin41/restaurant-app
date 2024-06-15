210de3ae9aa31e4fc2a48eb7423f0f2ff59c5fae
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-16 02:03:32 +0300

    Change endpoint path

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/controller/CustomerReviewController.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/controller/CustomerReviewController.java
@@ -12,7 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/reviews")
+@RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class CustomerReviewController {
 
