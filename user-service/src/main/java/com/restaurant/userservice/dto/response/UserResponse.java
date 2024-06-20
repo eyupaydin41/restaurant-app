4a77e5a1e003a5f32e3483a6a393fc17c1b79b96
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 18:17:31 +0300

    Updated UserResponse with UserType and restaurantList fields.

--- user-service/src/main/java/com/restaurant/userservice/dto/response/UserResponse.java
+++ user-service/src/main/java/com/restaurant/userservice/dto/response/UserResponse.java
@@ -1,18 +1,22 @@
 package com.restaurant.userservice.dto.response;
 
+import com.restaurant.userservice.model.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponse {
-    private Long id;
     private String name;
     private String surname;
     private String email;
     private String password;
+    private UserType userType;
+    private List<RestaurantResponse> restaurantList;
 }
