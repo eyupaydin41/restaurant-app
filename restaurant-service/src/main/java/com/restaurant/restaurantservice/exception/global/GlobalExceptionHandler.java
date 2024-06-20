20bbecd5b10519f37414752875fd71e3ed788566
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 17:57:41 +0300

    Update GlobalExceptionHandler to handle additional exceptions

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/exception/global/GlobalExceptionHandler.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/exception/global/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.restaurant.restaurantservice.exception.global;
 
-import com.restaurant.restaurantservice.exception.DuplicateReviewException;
-import com.restaurant.restaurantservice.exception.InvalidRatingException;
-import com.restaurant.restaurantservice.exception.RestaurantNotFoundException;
-import com.restaurant.restaurantservice.exception.ReviewNotFoundException;
+import com.restaurant.restaurantservice.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -33,6 +30,24 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidRatingException.class)
     public ResponseEntity<?> handleInvalidRatingException(InvalidRatingException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintException.class)
+    public ResponseEntity<?> handleConstraintException(ConstraintException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), ex.getErrors(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnauthorizedOperationException.class)
+    public ResponseEntity<?> handleUnauthorizedOperationException(UnauthorizedOperationException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
--- user-service/src/main/java/com/restaurant/userservice/exception/global/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/restaurant/userservice/exception/global/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.restaurant.userservice.exception.global;
 
+import com.restaurant.userservice.exception.ConstraintException;
+import com.restaurant.userservice.exception.DuplicateUserException;
 import com.restaurant.userservice.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintException.class)
+    public ResponseEntity<?> handleConstraintException(ConstraintException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), ex.getErrors(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<?> handleDuplicateUserException(DuplicateUserException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails,HttpStatus.BAD_REQUEST);
     }
 
 
