3be2f5fddc3ca42d1a5757c7cdba5372af469083
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 18:15:57 +0300

    Updated UserDTO with validation annotations and UserType field.

--- user-service/src/main/java/com/restaurant/userservice/dto/UserDTO.java
+++ user-service/src/main/java/com/restaurant/userservice/dto/UserDTO.java
@@ -1,17 +1,35 @@
 package com.restaurant.userservice.dto;
 
+import com.restaurant.userservice.model.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name cannot be empty")
     private String name;
+
+    @NotBlank(message = "Surname cannot be empty")
     private String surname;
+
+    @Email(message = "Email is not valid")
+    @NotBlank(message = "Email cannot be empty")
     private String email;
+
+    @NotBlank(message = "Password cannot be null")
     private String password;
+
+    @NotBlank(message = "UserType cannot be null")
+    private UserType userType;
 }
