acaf2c50b60a127d04c40eb34419a61adf81ef15
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 18:16:28 +0300

    Added UserRepository to establish database relationship.

---
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.restaurant.restaurantservice.repository;
+
+import com.restaurant.restaurantservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
