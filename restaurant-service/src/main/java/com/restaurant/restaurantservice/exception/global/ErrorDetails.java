067c9d1e23f7e50418fe66a32c730e36c3117622
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 17:56:43 +0300

    Updated ErrorDetails class

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/exception/global/ErrorDetails.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/exception/global/ErrorDetails.java
@@ -1,10 +1,12 @@
 package com.restaurant.restaurantservice.exception.global;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorDetails {
     private int statusCode;
     private String message;
