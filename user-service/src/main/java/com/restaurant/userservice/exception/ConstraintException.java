9a0e7d244d70f4377f3dc20abcbcd3614eb82ff8
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 17:40:10 +0300

    Add ConstraintException class for handling validation errors

---
+++ user-service/src/main/java/com/restaurant/userservice/exception/ConstraintException.java
@@ -0,0 +1,20 @@
+package com.restaurant.userservice.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ConstraintException extends RuntimeException{
+    private final List<String> errors;
+
+    public ConstraintException(List<String> errors) {
+        this.errors = errors;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
