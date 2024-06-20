78f4c395e032000258b194825ae08ab626abf1eb
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 18:14:00 +0300

    Added User entity to establish database relationship with restaurant-service.

---
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/model/User.java
@@ -0,0 +1,42 @@
+package com.restaurant.restaurantservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "user_type", nullable = false)
+    private UserType userType;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Column(name = "restaurants")
+    private List<Restaurant> restaurantList;
+
+}
