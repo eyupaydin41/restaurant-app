dd2bbdacbaf311471fde6f4e7f67a2efa493a837
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 17:55:48 +0300

    Updated ErrorDetails class

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/exception/global/ErrorDetails.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/exception/global/ErrorDetails.java
@@ -2,11 +2,14 @@ package com.restaurant.restaurantservice.exception.global;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ErrorDetails {
     private int statusCode;
     private String message;
     private String details;
+    private List<String> messageList;
 
     public ErrorDetails(int statusCode, String message, String details) {
         this.statusCode = statusCode;
@@ -15,4 +18,9 @@ public class ErrorDetails {
     }
 
 
+    public ErrorDetails(int statusCode, List<String> errors, String details) {
+        this.statusCode = statusCode;
+        this.messageList = errors;
+        this.details = details;
+    }
 }
--- user-service/src/main/java/com/restaurant/userservice/exception/global/ErrorDetails.java
+++ user-service/src/main/java/com/restaurant/userservice/exception/global/ErrorDetails.java
@@ -1,16 +1,27 @@
 package com.restaurant.userservice.exception.global;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorDetails {
     private int statusCode;
     private String message;
     private String details;
+    private List<String> messageList;
 
     public ErrorDetails(int statusCode, String message, String details) {
         this.statusCode = statusCode;
         this.message = message;
+        this.details = details;
+    }
+
+    public ErrorDetails(int statusCode, List<String> messageList, String details) {
+        this.statusCode = statusCode;
+        this.messageList = messageList;
         this.details = details;
     }
 
