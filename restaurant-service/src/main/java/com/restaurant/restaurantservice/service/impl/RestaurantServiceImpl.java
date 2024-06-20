9f49693467efd2fe2269b2b27b44a5e959dbfe23
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 18:11:30 +0300

    Update RestaurantServiceImpl with User Permissions and DTO Refinements
    - Updated 'createRestaurant' and 'updateRestaurant' methods to include user permission checks based on UserType.
    - Added 'UserRepository' dependency to fetch users for permission validation.
    - Introduced 'UnauthorizedOperationException' for unauthorized user actions during restaurant creation and update.
    - Updated 'deleteRestaurant' method to accept 'DeleteRestaurantRequest' and validate user permissions for deletion.
    - Refactored 'RestaurantDTO' to include 'userId' for association and validation purposes.

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/service/impl/RestaurantServiceImpl.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/service/impl/RestaurantServiceImpl.java
@@ -1,13 +1,19 @@
 package com.restaurant.restaurantservice.service.impl;
 
+import com.restaurant.restaurantservice.dto.DeleteRestaurantRequest;
 import com.restaurant.restaurantservice.dto.RestaurantDTO;
 import com.restaurant.restaurantservice.dto.response.RestaurantResponse;
 import com.restaurant.restaurantservice.dto.response.RestaurantWithReviewsResponse;
 import com.restaurant.restaurantservice.exception.RestaurantNotFoundException;
+import com.restaurant.restaurantservice.exception.UnauthorizedOperationException;
+import com.restaurant.restaurantservice.exception.UserNotFoundException;
 import com.restaurant.restaurantservice.model.Restaurant;
+import com.restaurant.restaurantservice.model.User;
+import com.restaurant.restaurantservice.model.UserType;
 import com.restaurant.restaurantservice.model.response.Response;
-import com.restaurant.restaurantservice.model.response.ResponseBuilder;
+import com.restaurant.restaurantservice.model.response.ResponseBuilderA;
 import com.restaurant.restaurantservice.repository.RestaurantRepository;
+import com.restaurant.restaurantservice.repository.UserRepository;
 import com.restaurant.restaurantservice.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -22,13 +28,24 @@ import java.util.List;
 public class RestaurantServiceImpl implements RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final UserRepository userRepository;
     private final ModelMapper modelMapper;
-    private final ResponseBuilder responseBuilder;
+    private final ResponseBuilderA responseBuilderA;
 
     @Override
     public ResponseEntity<Response> createRestaurant(RestaurantDTO restaurantDTO) {
+
+        User user = userRepository.findById(restaurantDTO.getUserId()).orElseThrow(
+                () -> new UserNotFoundException(restaurantDTO.getUserId())
+        );
+
+        if (user.getUserType().equals(UserType.STANDARD)) {
+            throw new UnauthorizedOperationException("The user not allowed to create restaurant");
+        }
+
         Restaurant restaurant = Restaurant.builder()
                 .name(restaurantDTO.getName())
+                .user(user)
                 .category(restaurantDTO.getCategory())
                 .address(restaurantDTO.getAddress())
                 .phone(restaurantDTO.getPhone())
@@ -42,7 +59,9 @@ public class RestaurantServiceImpl implements RestaurantService {
 
         restaurantRepository.save(restaurant);
 
-        return responseBuilder.buildResponse(HttpStatus.CREATED);
+        user.getRestaurantList().add(restaurant);
+
+        return responseBuilderA.buildResponse(HttpStatus.CREATED);
     }
 
     @Override
@@ -54,7 +73,7 @@ public class RestaurantServiceImpl implements RestaurantService {
 
         String message = "The restaurant list has been successfully fetched.";
 
-        return responseBuilder.buildResponse(message,HttpStatus.OK, list);
+        return responseBuilderA.buildResponse(message, HttpStatus.OK, list);
     }
 
 
@@ -67,14 +86,26 @@ public class RestaurantServiceImpl implements RestaurantService {
 
         String message = "Restaurant found with id: " + id;
 
-        return responseBuilder.buildResponse(message,HttpStatus.OK,response);
+        return responseBuilderA.buildResponse(message, HttpStatus.OK, response);
     }
 
     @Override
-    public ResponseEntity<Response> updateRestaurant(Long id, RestaurantDTO restaurantDTO) {
+    public ResponseEntity<Response> updateRestaurant(RestaurantDTO restaurantDTO) {
 
-        Restaurant restaurant = restaurantRepository.findById(id)
-                .orElseThrow(() -> new RestaurantNotFoundException(id));
+        Restaurant restaurant = restaurantRepository.findById(restaurantDTO.getId())
+                .orElseThrow(() -> new RestaurantNotFoundException(restaurantDTO.getId()));
+
+        User user = userRepository.findById(restaurantDTO.getUserId())
+                .orElseThrow(() -> new UserNotFoundException(restaurantDTO.getUserId()));
+
+        if (user.getUserType().equals(UserType.STANDARD)) {
+            throw new UnauthorizedOperationException("The user not allowed to update restaurant");
+        }
+
+        if (!restaurant.getUser().getId().equals(restaurantDTO.getUserId())) {
+            throw new UnauthorizedOperationException("The user not allowed to update this restaurant.");
+        }
+
 
         restaurant.setName(restaurantDTO.getName());
         restaurant.setDescription(restaurantDTO.getDescription());
@@ -85,24 +116,30 @@ public class RestaurantServiceImpl implements RestaurantService {
 
         restaurantRepository.save(restaurant);
 
-        return responseBuilder.buildResponse(HttpStatus.OK);
+        return responseBuilderA.buildResponse(HttpStatus.OK);
 
     }
 
     @Override
-    public ResponseEntity<Response> deleteRestaurant(Long id) {
-        restaurantRepository.findById(id).ifPresentOrElse(restaurantRepository::delete,
-                () -> {
-                    throw new RestaurantNotFoundException(id);
-                }
+    public ResponseEntity<Response> deleteRestaurant(DeleteRestaurantRequest deleteRestaurantRequest) {
+        Restaurant restaurant = restaurantRepository.findById(deleteRestaurantRequest.getRestaurantId()).orElseThrow(
+                () -> new RestaurantNotFoundException(deleteRestaurantRequest.getRestaurantId())
         );
 
-        return responseBuilder.buildResponse(HttpStatus.OK);
+        User user = userRepository.findById(deleteRestaurantRequest.getUserId())
+                .orElseThrow(() -> new UserNotFoundException(deleteRestaurantRequest.getUserId()));
+
+        if (!user.getUserType().equals(UserType.MANAGER)) {
+            throw new UnauthorizedOperationException("The user not allowed to delete restaurant");
+        }
+
+        restaurantRepository.delete(restaurant);
+
+        return responseBuilderA.buildResponse(HttpStatus.OK);
     }
 
     @Override
     public RestaurantResponse entityToDto(Restaurant restaurant) {
         return modelMapper.map(restaurant, RestaurantResponse.class);
     }
-
 }
