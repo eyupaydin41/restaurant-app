e1bf2bf2796113d52a9099446efd93dd8a856069
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 18:08:33 +0300

    Updated RestaurantDTO with ID and Validation Annotations

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/dto/RestaurantDTO.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/dto/RestaurantDTO.java
@@ -1,21 +1,36 @@
 package com.restaurant.restaurantservice.dto;
 
 import com.restaurant.restaurantservice.model.RestaurantCategory;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RestaurantDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name cannot be null")
     private String name;
+
+    @NotNull(message = "UserID cannot be null")
+    private Long userId;
+
+    @NotNull(message = "Category cannot be null")
     private RestaurantCategory category;
+
+    @NotBlank(message = "Address cannot be null")
     private String address;
+
+    @NotBlank(message = "Phone cannot be null")
     private String phone;
+
+    @NotBlank(message = "Email cannot be null")
     private String email;
+
     private String description;
 
 }
