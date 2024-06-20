51fa6c1c6ca2ac62ce412658104a54be46a9e081
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-20 18:02:33 +0300

    Rename ResponseBuilder to ResponseBuilderA to avoid bean name conflict

--- user-service/src/main/java/com/restaurant/userservice/model/response/ResponseBuilderA.java
+++ user-service/src/main/java/com/restaurant/userservice/model/response/ResponseBuilderA.java
@@ -1,11 +1,11 @@
-package com.restaurant.restaurantservice.model.response;
+package com.restaurant.userservice.model.response;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ResponseBuilder {
+public class ResponseBuilderA {
     public ResponseEntity<Response> buildResponse(String message, HttpStatus status, Object data) {
         Response response = new Response(status, message, data);
         return ResponseEntity.status(status).body(response);
