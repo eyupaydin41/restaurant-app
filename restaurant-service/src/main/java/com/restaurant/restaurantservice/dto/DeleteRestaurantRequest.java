822927a09ca4cefdf7754fb2a7350ba5e9d36f29
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-06-24 00:30:15 +0300

    Added @NoArgsConstructor annotation

--- restaurant-service/src/main/java/com/restaurant/restaurantservice/dto/DeleteRestaurantRequest.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/dto/DeleteRestaurantRequest.java
@@ -1,8 +1,10 @@
 package com.restaurant.restaurantservice.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class DeleteRestaurantRequest {
 
     private Long userId;
--- restaurant-service/src/main/java/com/restaurant/restaurantservice/dto/DeleteReviewRequest.java
+++ restaurant-service/src/main/java/com/restaurant/restaurantservice/dto/DeleteReviewRequest.java
@@ -1,8 +1,11 @@
 package com.restaurant.restaurantservice.dto;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class DeleteReviewRequest {
     private Long userId;
     private Long reviewId;
